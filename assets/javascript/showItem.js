3f0bf263c83d7594257d85ac8cc2793367c2cd82
xuanthang205 <thang652014@gmail.com>
2025-01-15 02:16:00 +0700

    fix slide courses/countdown

---
+++ assets/javascript/showItem.js
@@ -0,0 +1,55 @@
+const coursesBody = document.querySelector(".courses-body");
+const viewBtn = document.querySelector(".courses-top .cta");
+const hideBtn = document.querySelector(".courses .hide");
+
+let swiper = new Swiper(".courses-body", {
+    slidesPerView: 3,
+    spaceBetween: 47,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".btn-next", // Cập nhật với tên class mới
+        prevEl: ".btn-prev", // Cập nhật với tên class mới
+    },
+    pagination: {
+        el: ".pagination",
+        clickable: true,
+    },
+});
+
+viewBtn.onclick = function () {
+    if (swiper) {
+        swiper.destroy(true, true); // Tham số: removeSwiperWrapper và cleanStyles
+        swiper = null;
+    }
+    coursesBody.classList.add("show");
+    viewBtn.style.display = "none";
+};
+
+hideBtn.onclick = function () {
+    coursesBody.classList.remove("show");
+    viewBtn.style.display = "block";
+    if (!swiper) {
+        // Kiểm tra nếu Swiper chưa được khởi tạo
+        swiper = new Swiper(".courses-body", {
+            slidesPerView: 3,
+            spaceBetween: 47,
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            navigation: {
+                nextEl: ".btn-next", // Cập nhật với tên class mới
+                prevEl: ".btn-prev", // Cập nhật với tên class mới
+            },
+            pagination: {
+                el: ".pagination",
+                clickable: true,
+            },
+        });
+    }
+};
